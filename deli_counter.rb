57fbce84fe25bcd415ae43dcc90dce4afb77852f
Terren Culberson <culbersonterren@yahoo.com>
2019-12-19 02:52:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,10 @@ def take_a_number(array, name)
   return name, position 
 end   
 
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
