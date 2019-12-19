ac0952f533910dec763df245a85a962cbbc93a42
Terren Culberson <culbersonterren@yahoo.com>
2019-12-19 02:35:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,4 @@
 # Write your code here.
 def line(array)
-  if array.length >= l
-    nuarray = []
-    counter == 1 
-    array.each do |name|
-      nuarray.push("#{counter} #{name}")
-      counter += l
-    end
-    puts "The line is currently: #{nuarray.join(" ")}"
-  else
-    puts "The line is currenlty empty."
-   end
-  end
+  if array.length >= 
   
